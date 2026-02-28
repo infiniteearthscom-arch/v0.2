9905c2f1e3e65a9cebc0d4ab2a8520d2fb279c86
JS-GSV <jason.simons@greatersumventures.com>
2026-02-27 23:40:32 -0500

    fixes

--- star-shipper-server/src/db/index.js
+++ star-shipper-server/src/db/index.js
@@ -8,7 +8,7 @@ const { Pool } = pg;
 // Create connection pool
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+ssl: process.env.DATABASE_URL ? { rejectUnauthorized: false } : false,
   max: 20,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
